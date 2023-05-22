f8ced1e63a466da0b997a518875075b58b67352c
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-05-22 02:45:07 -0300

    fix:rota de cadastro aceitando novos cadastros

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -6,7 +6,7 @@ export async function postCadastro(req, res){
    const { name, email, password, confirmPassword } = req.body
 
    try{
-    const emailExiste = await db.query(`SELECT email FROM users`)
+    const emailExiste = await db.query(`SELECT email FROM users WHERE email=$1`, [email])
     if(emailExiste.rows.length!==0){
         return res.status(409).send("Email já possui cadastro")
     }
